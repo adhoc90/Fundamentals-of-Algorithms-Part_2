8dcaee51915a2cfbb371f7c8ce34f0753fd82b98
ad_hoc <ad_hoc90@mail.ru>
2023-08-23 22:14:19 +0300

    initial commit

---
+++ src/main/java/ru/pro/sky/fundamentalsOfAlgorithmsPart2/FundamentalsOfAlgorithmsPart2Application.java
@@ -0,0 +1,13 @@
+package ru.pro.sky.fundamentalsOfAlgorithmsPart2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentalsOfAlgorithmsPart2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundamentalsOfAlgorithmsPart2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/pro/sky/fundamentalsOfAlgorithmsPart2/FundamentalsOfAlgorithmsPart2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pro.sky.fundamentalsOfAlgorithmsPart2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentalsOfAlgorithmsPart2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
